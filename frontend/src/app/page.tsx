fe9613142b5329c007b529283d3dab0710fcb236
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-01-30 00:19:19 +0700

    แก้นิดแก้หน่อย from guncv

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex text-[100px] w-screen justify-center items-center h-screen">
       PUBPAB
     </main>
-  );
+  )
 }
