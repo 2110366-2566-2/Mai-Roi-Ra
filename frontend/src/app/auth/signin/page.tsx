b0f6017144be54db6406f684d49716093bea0358
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-02-02 02:10:47 +0700

    Responsive for signin page

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -39,40 +39,44 @@ export default function SignIn() {
     return (
         <div className="text-black bg-white w-screen h-screen flex justify-center items-center">
 
-            <div className={`${styles.Actor} text-center w-[450px] h-[450px]`}>
+            <div className={`${styles.Roboto} text-center lg:w-[450px] lg:h-[450px] md:w-[350px] md:h-[350]`}>
 
-                <div className="w-[60px] h-[60px]">
+                <div className="lg:w-[60px] lg:h-[60px] md:w-[50px] md:h-[50px] sm:w-[40px] sm:h-[40px] h-[30px] w-[30px]">
                         <Image src="/img/icon_sunlight.png"
                         alt="Failed To Load Image"
                         width={1000}
                         height={1000}/>
                 </div>
 
-                <div className={`${styles.Roboto} w-full text-[42px] mt-[10px] font-black`}>
+                <div className="w-full lg:text-[42px] md:text-[35px] sm:text-[30px] text-[25px] mt-[10px] font-black">
                     Log in to MAI-ROI-RA
                 </div>
 
-                <div className="mt-[20px] w-full text-[18px]">
+                <div className="mt-[20px] w-full lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]">
                     <form onSubmit={handleSubmit}>   
                         <div>
-                            <input className="w-full h-[60px] rounded-md border-gray-200 border-[1px] indent-[20px]" 
+                            <input className="w-full lg:h-[60px] md:h-[55px] sm:h-[50px] h-[45px] rounded-md border-gray-200 
+                            border-[1px] indent-[20px]" 
                             type="text" placeholder="Phone number, email address" onChange={(e) => {setUser(e.target.value)}}/> 
                         </div>
 
                         <div className="mt-[20px]">
-                            <input className="w-full h-[60px] rounded-md border-gray-200 border-[1px] indent-[20px]" 
+                            <input className="w-full lg:h-[60px] md:h-[55px] sm:h-[50px] h-[45px] rounded-md border-gray-200 
+                            border-[1px] indent-[20px]" 
                             type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}}/>
                         </div>
                     </form>
                 </div>
 
                 <div className="mt-[20px]">
-                    <button type="submit" className="rounded-full text-white hover:bg-sky-600 bg-sky-500 w-full h-[50px] text-[18px]">
+                    <button type="submit" className="rounded-full text-white hover:bg-sky-600 bg-sky-500 w-full 
+                    lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px] lg:h-[50px] md:h-[45px] sm:h-[40px] h-[35px]">
                         Log in
                     </button>
                 </div>
 
-                <div className="flex justify-end w-full text-sky-500 mt-[30px] text-[18px]">
+                <div className="flex justify-end w-full text-sky-500 mt-[30px] 
+                    lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px]">
                     <Link href="/auth/register" className="hover:text-sky-600 hover:underline">
                         Sign up
                     </Link>
