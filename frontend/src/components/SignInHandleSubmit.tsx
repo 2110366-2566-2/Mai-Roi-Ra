e5fc9fe8f3296dc0491021c6e62be12774af3e3a
Chanagun Viriyasathapornpong <chanagun.vir@gmail.com>
2024-02-02 18:48:28 +0700

    comment

--- frontend/src/components/SignInHandleSubmit.tsx
+++ frontend/src/components/SignInHandleSubmit.tsx
@@ -2,27 +2,34 @@ import { FormEvent } from "react";
 
 const SignInHandleSubmit = async (e: FormEvent, setUser: Function, setPassword: Function, 
 setError: Function, setErrorUser: Function, setErrorPassword: Function, user: string, password: string, router: any) => {
+  // Set Default
     e.preventDefault();
     setError(false);
     setErrorUser(0);
     setErrorPassword(0);
 
+  // Check User Input
   if (user === "") {
     setErrorUser(1);
     setError(true);
     setPassword("");
     console.log("Error: Please fill in your phone number or email address");
-  } else if (password === "") {
+  } 
+  // Check Password Input
+  else if (password === "") {
     setErrorPassword(1);
     setError(true);
     setPassword("");
     console.log("Error: Please fill in your password");
-  } else if (user.length < 10 || user.slice(-10) !== "@gmail.com" || (user.length === 10 && user[0] === '0' && !isNaN(Number(user)))) {
+  } 
+  // Check User That Gmail Or Phone Number Format?
+  else if (user.length < 10 || user.slice(-10) !== "@gmail.com" || (user.length === 10 && user[0] === '0' && !isNaN(Number(user)))) {
     setErrorUser(2);
     setError(true);
     setPassword("");
     console.log("Error: Not a phone number or email address format");
-  } else {
+  } 
+  else {
     // Your authentication logic here
     // For example, you can use the signIn function from next-auth/react
     // and handle the redirect or error accordingly
