013ed2441def311845656389aa4629f762fe5f14
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2024-02-13 13:35:19 +0700

    add router to profile page

--- frontend/src/components/ProfilePage.tsx
+++ frontend/src/components/ProfilePage.tsx
@@ -7,6 +7,7 @@ import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { PiBalloonBold } from "react-icons/pi";
 import { SlLocationPin } from "react-icons/sl";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const [username, setUsername] = useState("wish-eq");
@@ -69,6 +70,13 @@ export default function ProfilePage() {
     },
   ];
 
+  const router = useRouter();
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    router.push("/editprofile");
+  };
+
   return (
     <div className="bg-white text-black h-full">
       <div className="lg:mr-24 border-r bg-white">
@@ -128,7 +136,10 @@ export default function ProfilePage() {
             </div>
             <div className="w-full">
               <div className="absolute top-0 right-0 m-4">
-                <button className="text-sm bg-white hover:bg-gray-100 py-2 px-4 rounded-full border border-gray-300">
+                <button
+                  className="text-sm bg-white hover:bg-gray-100 py-2 px-4 rounded-full border border-gray-300"
+                  onClick={handleClick}
+                >
                   Edit Profile
                 </button>
               </div>
