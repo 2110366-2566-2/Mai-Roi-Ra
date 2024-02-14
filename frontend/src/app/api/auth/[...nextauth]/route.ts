831723befb8951d5bd08bf5dfdca06ed3d8e470b
tanawinvisa <tanawinvisa@gmail.com>
2024-02-15 02:03:31 +0700

    Revert "Try hard to nextauth but waste super time"
    This reverts commit 10515c791afc7aae8c12e98491432bb6946c18e6.

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -19,18 +19,17 @@ export const authOptions:AuthOptions = {
             },
             async authorize(credentials, req) {
               
-              if (!credentials) {
+              if (!credentials?.username || !credentials.password || !credentials) {
                 throw new Error("Please fill email and password.");
               }
 
               const user = await userLogin(credentials.username, credentials.password);
+              
               if (user) {
                 // Any object returned will be saved in `user` property of the JWT
                 return user
               } else {
                 // If you return null then an error will be displayed advising the user to check their details.
-                throw new Error("that what you look")
-
                 return null
         
                 // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
--- frontend/src/libs/userLogin.tsx
+++ frontend/src/libs/userLogin.tsx
@@ -1,30 +1,19 @@
 import { apiBackUrl } from "../constants"
 
-const isValidEmail = (email: string):boolean => {
-    const emailRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegex.test(email.toLowerCase());
-};
-
-export default async function userLogin(username: string, password:string) {
-    console.log(username,password)
-    
+export default async function userLogin(userName: string, userPassword:string) {
     const response = await fetch(`${apiBackUrl}/auth/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            email: isValidEmail(username) ? username : null,
-            phonenumber: (username.length === 10 && username[0] === '0') ? username : null,
-            password: password
+            username: userName,
+            password: userPassword
         }),
     })
     if (!response.ok) {
         throw new Error("Failed to log in")
     }
-
-    console.log(response.json());
 
     return await response.json()
 }
