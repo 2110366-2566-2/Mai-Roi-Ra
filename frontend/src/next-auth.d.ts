4bbeb7c0696d750599f2d4c24a0a84d9d49ce118
tanawinvisa <tanawinvisa@gmail.com>
2024-02-22 01:50:19 +0700

    add modal, check if deadline isnt pass

--- frontend/src/next-auth.d.ts
+++ frontend/src/next-auth.d.ts
@@ -3,7 +3,7 @@ import NextAuth from 'next-auth'
 declare module "next-auth" {
 	interface Session {
 		user: {
-			_id: string,
+			user_id: string,
 			name: string,
 			email: string,
 			role: string,
