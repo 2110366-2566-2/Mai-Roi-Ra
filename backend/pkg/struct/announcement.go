b46cef296e3d38b13cfcad71908b70d87b8c713b
Kathayut Kannasoot <97604415+Jdomenusrex@users.noreply.github.com>
2024-02-21 16:36:44 +0700

    remove unused binding

--- backend/pkg/struct/announcement.go
+++ backend/pkg/struct/announcement.go
@@ -3,10 +3,10 @@ package structure
 import "time"
 
 type Announcement struct {
-	Header      string    `json:"header" binding:"required"`
-	Description string    `json:"description" binding:"required"`
-	CreatedAt   time.Time `json:"created_at" binding:"required"`
-	UpdatedAt   time.Time `json:"updated_at" binding:"required"`
+	Header      string    `json:"header"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
 
 type GetAnnouncementListsRequest struct {
