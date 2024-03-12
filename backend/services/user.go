72e38b202aade48371593081f13ec14d029753d0
NUT <natthasith.wir@gmail.com>
2024-03-13 00:50:27 +0700

    logout fixed

--- backend/services/user.go
+++ backend/services/user.go
@@ -358,7 +358,7 @@ func (s *UserService) ValidateToken(token string) (*models.User, error) {
 // GenerateJWTToken generates a new JWT token for authenticated users
 func GenerateJWTToken(user *models.User) (string, error) {
 	log.Println("[Service: GenerateJWTToken]: Called")
-	var secretKey = []byte("secret-key")
+	var secretKey = []byte("YourSecretKey")
 	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 	// 	"user_id":  user.UserID,                           // Include the user's ID
 	// 	"username": user.Username,                         // Include the username
@@ -421,7 +421,7 @@ func (s *UserService) RegisterEvent(req *st.RegisterEventRequest) (*st.RegisterE
 		EventName:   resevent.EventName,
 	}
 
-	if _,err := announcementService.SendRegisteredEmail(reqregister); err != nil {
+	if _, err := announcementService.SendRegisteredEmail(reqregister); err != nil {
 		log.Println("[Service: Call SendRegisteredEmail]:", err)
 		return nil, err
 	}
