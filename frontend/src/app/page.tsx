5a8bd43c518988f8c2081b912ebafd6d83df38e5
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2024-03-19 15:53:33 +0700

    move UserHompage into component
    - move homepage -> page.tsx to <UserHomepage> as we need to check if user is ADMIN or not

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import styles from "@/styles/page.module.css"
-import HomePage from '@/components/HomePage'
+import Image from "next/image";
+import styles from "@/styles/page.module.css";
+import HomePage from "@/components/HomePage";
 
 export default function Home() {
   return (
     <main className="bg-white text-black">
-      <HomePage/>
+      <HomePage />
     </main>
-  )
+  );
 }
