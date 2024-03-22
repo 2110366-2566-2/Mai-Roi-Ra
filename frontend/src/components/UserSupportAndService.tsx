ccf37199b80b2b3f660d0a27d3987dbeceee7596
guncv <chanagun.vir@gmail.com>
2024-03-22 15:54:12 +0700

    commit

--- frontend/src/components/UserSupportAndService.tsx
+++ frontend/src/components/UserSupportAndService.tsx
@@ -1,11 +1,12 @@
+'use client'
 import Link from "next/link";
-import React, { useState } from "react";
-import FAQlist from "./FAQlist";
-import ProblemList from "./ProblemList";
-import ReportProblemPage from "./ReportProblemPage";
-// Other imports...
+import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function UserSupportAndService() {
+  const pathname = usePathname();
+
+  console.log(pathname);
 
   return (
     <div className="bg-white text-black h-full">
@@ -17,13 +18,19 @@ export default function UserSupportAndService() {
               className={`relative px-8 py-2 overflow-hidden`}
             >
               <span
-                className={`transition duration-500 ease-in-out
+                className={`transition duration-500 ease-in-out ${
+                  pathname === `supportandservice/${tabName.toLowerCase()}`
+                    ? "text-yellow-500"
+                    : "text-gray-800 hover:text-gray-500"
+                }
                 `}
               >
                 {tabName}
               </span>
               <span
-                className={`absolute inset-x-0 bottom-0 h-0.5 bg-yellow-500 transition-all duration-500 ease-out`}
+                className={`absolute inset-x-0 bottom-0 h-0.5 bg-yellow-500 transition-all duration-500 ease-out ${
+                  pathname === `supportandservice/${tabName.toLowerCase()}` ? "scale-x-100" : "scale-x-0"
+                }`}
                 style={{
                   transformOrigin: "center",
                 }}
