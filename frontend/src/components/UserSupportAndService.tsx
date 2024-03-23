6f4593e4af80b75f4771f7a58b3c04bfca630d96
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2024-03-23 22:41:52 +0700

    edit path

--- frontend/src/components/UserSupportAndService.tsx
+++ frontend/src/components/UserSupportAndService.tsx
@@ -1,41 +1,42 @@
-'use client'
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+"use client";
 import React, { useState } from "react";
-import FAQlist from "./FAQlist";
-import ProblemList from "./ProblemList";
-import ReportProblemPage from "./ReportProblemButton";
 // Other imports...
 
 export default function UserSupportAndService() {
-  const pathname = usePathname();
+  const [activeTab, setActiveTab] = useState("FAQ");
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="bg-white text-black h-full">
       <div className="lg:mr-24 border-r border-b bg-white">
         <div className="w-full text-2xl pt-20">
           {["FAQ", "Problem"].map((tabName) => (
-            <Link href={`/supportandservice/${tabName.toLowerCase()}`}
+            <button
               key={tabName}
               className={`relative px-8 py-2 overflow-hidden`}
+              onClick={() => handleTabClick(tabName)}
             >
               <span
                 className={`transition duration-500 ease-in-out ${
-                  pathname == `/supportandservice/${tabName.toLowerCase()}` ?
-                    "text-yellow-500"
+                  activeTab === tabName
+                    ? "text-yellow-500"
                     : "text-gray-800 hover:text-gray-500"
                 }`}
               >
                 {tabName}
               </span>
               <span
-                 className={`absolute inset-x-0 bottom-0 h-0.5 bg-yellow-500 transition-all duration-500 ease-out ${
-                  pathname == `/supportandservice/${tabName.toLowerCase()}` ? "scale-x-100" : "scale-x-0"
+                className={`absolute inset-x-0 bottom-0 h-0.5 bg-yellow-500 transition-all duration-500 ease-out ${
+                  activeTab === tabName ? "scale-x-100" : "scale-x-0"
                 }`}
                 style={{
                   transformOrigin: "center",
                 }}
               ></span>
-            </Link>
+            </button>
           ))}
         </div>
       </div>
