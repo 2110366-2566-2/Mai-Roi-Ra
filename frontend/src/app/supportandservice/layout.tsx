46fab99566f5b4d7b40f3164184cf292298951da
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2024-03-23 22:38:24 +0700

    edit supportandservice homepage

--- frontend/src/app/supportandservice/layout.tsx
+++ frontend/src/app/supportandservice/layout.tsx
@@ -2,33 +2,18 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import MenuBar from "@/components/MenuBar";
 import Menu from "@/components/Menu";
-import AdminSupportAndService from "@/components/AdminSupportAndService";
-import UserSupportAndService from "@/components/UserSupportAndService";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession(authOptions);
-  console.log("successfully");
-  
-  console.log(session);
   return (
     <html lang="en">
       <body className={`{inter.className} h-screen text-black`}>
         <MenuBar />
         <Menu />
-        <div className="lg:pl-[20%] md:mt-0 mt-[20px]">
-          {session?.user.role == "ADMIN" ? (
-          <AdminSupportAndService></AdminSupportAndService>
-        ) : (
-          <UserSupportAndService></UserSupportAndService>
-        )}
-         {children}
-        </div>
+        <div className="lg:pl-[20%] md:mt-0 mt-[20px]">{children}</div>
       </body>
     </html>
   );
