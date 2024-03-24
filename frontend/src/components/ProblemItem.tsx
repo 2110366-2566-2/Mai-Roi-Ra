7b1cf2d5f62dbe85961c1d838076cfdb26134e46
nattoshi <97926284+nattoshi@users.noreply.github.com>
2024-03-24 23:54:04 +0700

    popupPage done!!!!!

--- frontend/src/components/ProblemItem.tsx
+++ frontend/src/components/ProblemItem.tsx
@@ -9,24 +9,34 @@ interface Props {
   problem: string;
   description: string;
   status: string;
+  reply: string
 }
 
-export default function ProblemItem({ id, problem, description ,status}: Props) {
+export default function ProblemItem({ id, problem, description, status, reply }: Props) {
   const [showModal, setShowModal] = useState(false);
   const router = useRouter();
 
   return (
     <div className="relative"> {/* Container with relative positioning */}
-      <div className="absolute top-0 right-0 mt-2 mr-2 bg-white text-black px-2 py-1 rounded" style={{ border: '1px solid black' }}>
-          {status}
-      </div> {/* Status indicator */}
-      <div className="flex items-center my-4 shadow-md lg:h-[200px] md:h-[160px] h-[120px] p-2 lg:p-10 w-full hover:scale-105 duration-300">
-        <div className="h-full flex flex-col justify-start w-full space-y-[7px] mx-10">
-          <div className="flex justify-between">
+      <div className="flex items-center my-4 shadow-md lg:h-[200px] md:h-[160px] h-[120px] p-2 lg:p-10 w-full hover:scale-105 duration-300" >
+        <div className="h-full flex flex-col justify-start w-full space-y-[7px] mx-10"
+        onClick={(e) => {
+          e.stopPropagation();
+          e.preventDefault();
+          setShowModal(true);
+        }} >
+          <div className="flex justify-between" >
             <h2 className="lg:text-2xl md:text-xl sm:text-md text-md font-semibold">
               {problem}
             </h2>
+            <div className="space-x-2">
+              <button
+                className="border border-slate-400 rounded-xl h-[30px] w-[80px] text-sm hover:scale-105 duration-300">
+                {status}
+              </button>
+            </div>
           </div>
+
           <div className="">
             {description && (
               <div className="hidden sm:block break-words">
@@ -35,7 +45,9 @@ export default function ProblemItem({ id, problem, description ,status}: Props) 
             )}
           </div>
         </div>
+
       </div>
+      <ProblemPopUp isVisible={showModal} onClose={() => setShowModal(false)} id={id} problem={problem} description={description} reply={reply} />
     </div>
   );
 }
