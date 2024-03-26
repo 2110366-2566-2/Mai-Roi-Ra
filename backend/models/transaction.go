b8d55fc214b22a28c3146514abcca6b5652ef062
Jirawat14 <natchjirawat@gmail.com>
2024-03-26 15:19:54 +0700

    fix: integrate update status to GetPayment

--- backend/models/transaction.go
+++ backend/models/transaction.go
@@ -7,6 +7,7 @@ import (
 type Transaction struct {
 	TransactionID     string    `gorm:"column:transaction_id;primaryKey" json:"transaction_id"`
 	UserID            string    `gorm:"column:user_id;not null" json:"user_id" binding:"required"`
+	EventID           string    `gorm:"column:event_id;not null" json:"event_id" binding:"required"`
 	PaymentIntentID   string    `gorm:"column:payment_intent_id;not null" json:"payment_intent_id" binding:"required"`
 	TransactionAmount float64   `gorm:"column:transaction_amount" json:"transaction_amount" binding:"required"`
 	TransactionDate   time.Time `gorm:"column:transaction_date;autoCreateTime" json:"transaction_date"`
