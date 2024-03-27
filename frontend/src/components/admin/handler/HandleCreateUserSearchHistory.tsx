f8d20557ae6dc54bf365b916b501a339ff81f503
guncv <chanagun.vir@gmail.com>
2024-03-27 16:37:29 +0700

    search event and loading announcement pop up during backend send email

---
+++ frontend/src/components/admin/handler/HandleCreateUserSearchHistory.tsx
@@ -0,0 +1,23 @@
+'use server'
+import createUserSearchHistory from "@/libs/createUserSearchHistory";
+import { revalidatePath, revalidateTag } from "next/cache"
+import { redirect } from "next/navigation";
+
+export async function HandleCreateUserSearchHistory(user_id:string,search:string,token:string){
+    console.log(token)
+    try {
+        const res = await createUserSearchHistory(user_id,search,token);
+        console.log(user_id,search);
+        console.log(res);
+        console.log("Create Searching History successful");
+    } catch (err) {
+        console.log("Error during creating search history : ", err)
+    }
+    revalidateTag(`searchhistory`);
+    revalidatePath(`/homepage?search=${search}`);
+    revalidatePath(`/homepage`);
+    redirect(`/homepage?search=${search}`);
+}
+
+
+
