faac4e53226a6a88c187ad34d2569a00b46a39d0
tanawinvisa <tanawinvisa@gmail.com>
2024-03-27 18:55:12 +0700

    push /homepage in EventDetailpage

--- frontend/src/components/RouterBackEventButton.tsx
+++ frontend/src/components/RouterBackEventButton.tsx
@@ -2,11 +2,12 @@
 import { useRouter } from "next/navigation"
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 
-export default function RouterBackEventButton(){
+export default function RouterBackEventButton({isEventDetailPage}: {isEventDetailPage: boolean}){
     const router = useRouter();
 
     return (
-        <div className="absolute top-[38px] left-8 flex items-center justify-center hover:scale-[1.2] duration-300" onClick={()=>router.push('/homepage')}>
+        <div className="absolute top-[38px] left-8 flex items-center justify-center hover:scale-[1.2] duration-300" 
+        onClick={()=>{isEventDetailPage ? router.push('/homepage') :router.back()}}>
                 <ArrowBackIosNewIcon className="text-[#1DA1F2]"/>
         </div>
     )
