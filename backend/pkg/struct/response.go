1d26e0133398f5f0ba05cbbb0662271fd40d2e40
Kathayut Kannasoot <97604415+Jdomenusrex@users.noreply.github.com>
2024-04-07 22:08:59 +0700

    add GetResponseByPostId

--- backend/pkg/struct/response.go
+++ backend/pkg/struct/response.go
@@ -1,9 +1,19 @@
 package structure
 
+type GetResponseByPostIdRequest struct {
+	PostId string `json:"post_id" binding:"required"`
+}
+
+type GetResponseByPostIdResponse struct {
+	OrganizerId string `json:"organizer_id"`
+	PostId      string `json:"post_id"`
+	Detail      string `json:"detail"`
+}
+
 type CreateResponseRequest struct {
-	OrganizerId string    `json:"organizer_id" binding:"required"`
-	PostId      string    `json:"post_id" binding:"required"`
-	Detail      string    `json:"detail"`
+	OrganizerId string `json:"organizer_id" binding:"required"`
+	PostId      string `json:"post_id" binding:"required"`
+	Detail      string `json:"detail"`
 }
 
 type CreateResponseResponse struct {
