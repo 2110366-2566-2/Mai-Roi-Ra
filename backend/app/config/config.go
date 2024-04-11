d3bf621fd78d2eea3a91e39a8d779c95eba521c0
Jirawat14 <natchjirawat@gmail.com>
2024-04-12 00:17:14 +0700

    chore: add aws secret manager

--- backend/app/config/config.go
+++ backend/app/config/config.go
@@ -1,9 +1,14 @@
 package config
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/joho/godotenv"
 )
 
@@ -70,10 +75,42 @@ func NewConfig(path string) (*Config, error) {
 		log.Println("Error loading .env file: ", err)
 		return nil, err
 	}
+
+	// Retrieve the secret values from AWS Secrets Manager
+	secretName := os.Getenv("AWS_SECRET_MANAGER_SECRET_NAME")
+	region := os.Getenv("AWS_SECRET_MANAGER_REGION")
+
+	// Load AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(region))
+	if err != nil {
+		log.Println("Error loading AWS config: ", err)
+		return nil, err
+	}
+
+	// Create a Secrets Manager client
+	client := secretsmanager.NewFromConfig(cfg)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+	result, err := client.GetSecretValue(context.Background(), input)
+	if err != nil {
+		log.Println("Error retrieving secret from AWS Secrets Manager: ", err)
+		return nil, err
+	}
+	// Parse the secret value
+	var secretData map[string]string
+	if err := json.Unmarshal([]byte(*result.SecretString), &secretData); err != nil {
+		log.Println("Error parsing secret data: ", err)
+		return nil, err
+	}
+
+	// Now, replace the hard-coded values with the secret values
 	return &Config{
 		App: &App{
-			Url:     os.Getenv("SERVER_HOST"),
-			AppName: os.Getenv("APP_NAME"),
+			Url:     secretData["SERVER_HOST"],
+			AppName: secretData["APP_NAME"],
 		},
 		MongoDB: &MongoDB{
 			Uri:      os.Getenv("MONGODB_URI"),
@@ -81,35 +118,35 @@ func NewConfig(path string) (*Config, error) {
 			Password: os.Getenv("MONGODB_PASSWORD"),
 		},
 		PgDB: &PgDB{
-			Username: os.Getenv("PG_USER"),
-			Password: os.Getenv("PG_PASSWORD"),
-			DbName:   os.Getenv("PG_DB"),
+			Username: secretData["PG_USER"],
+			Password: secretData["PG_PASSWORD"],
+			DbName:   secretData["PG_DB"],
 		},
 		Email: &Email{
-			Name:     os.Getenv("EMAIL_SENDER_NAME"),
-			Address:  os.Getenv("EMAIL_SENDER_ADDRESS"),
-			Password: os.Getenv("EMAIL_SENDER_PASSWORD"),
+			Name:     secretData["EMAIL_SENDER_NAME"],
+			Address:  secretData["EMAIL_SENDER_ADDRESS"],
+			Password: secretData["EMAIL_SENDER_PASSWORD"],
 		},
 
 		GoogleAuth: &GoogleAuth{
-			ClientId:     os.Getenv("GOOGLE_CLIENT_ID"),
-			ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-			CallbackURL:  os.Getenv("GOOGLE_CALLBACK_URL"),
+			ClientId:     secretData["GOOGLE_CLIENT_ID"],
+			ClientSecret: secretData["GOOGLE_CLIENT_SECRET"],
+			CallbackURL:  secretData["GOOGLE_CALLBACK_URL"],
 		},
 		S3: &S3{
-			AwsRegion:            os.Getenv("AWS_REGION"),
-			AwsAccessKeyID:       os.Getenv("AWS_ACCESS_KEY_ID"),
-			AwsSecretKey:         os.Getenv("AWS_SECRET_ACCESS_KEY"),
-			AwsBucketProfileName: os.Getenv("AWS_BUCKET_PROFILE_NAME"),
-			AwsBucketEventName:   os.Getenv("AWS_BUCKET_EVENT_NAME"),
+			AwsRegion:            secretData["AWS_REGION"],
+			AwsAccessKeyID:       secretData["AWS_ACCESS_KEY_ID"],
+			AwsSecretKey:         secretData["AWS_SECRET_ACCESS_KEY"],
+			AwsBucketProfileName: secretData["AWS_BUCKET_PROFILE_NAME"],
+			AwsBucketEventName:   secretData["AWS_BUCKET_EVENT_NAME"],
 		},
 		Omise: &Omise{
-			PublicKey: os.Getenv("OMISE_PUBLIC_KEY"),
-			SecretKey: os.Getenv("OMISE_SECRET_KEY"),
+			PublicKey: secretData["OMISE_PUBLIC_KEY"],
+			SecretKey: secretData["OMISE_SECRET_KEY"],
 		},
 		Stripe: &Stripe{
-			PublicKey: os.Getenv("STRIPE_PUBLIC_KEY"),
-			SecretKey: os.Getenv("STRIPE_SECRET_KEY"),
+			PublicKey: secretData["STRIPE_PUBLIC_KEY"],
+			SecretKey: secretData["STRIPE_SECRET_KEY"],
 		},
 	}, nil
 }
