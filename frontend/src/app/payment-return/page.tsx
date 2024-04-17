f6c2a6880abc374261167fea383be3eb718f399a
tanawinvisa <tanawinvisa@gmail.com>
2024-04-17 22:32:00 +0700

    payment-return/page.tsx

--- frontend/src/app/payment-return/page.tsx
+++ frontend/src/app/payment-return/page.tsx
@@ -8,9 +8,19 @@ import isRegisteredEvent from '@/libs/isRegisteredEvent';
 import LoadingLine from '@/components/LoadingLine';
 
 
+type PaymentIntent = {
+  PaymentIntentId: string;
+  TransactionAmount: number;
+  Currency: string;
+  Status: string;
+  EventId: string;
+  NumParticipants: number;
+  UserId: string;
+};
+
 const StripeReturnPage = () => {
   const { data: session } = useSession();
-  const [paymentIntent, setPaymentIntent] = useState("");
+  const [paymentIntent, setPaymentIntent] = useState(null as PaymentIntent | null);
   const router = useRouter();
   const [isRegisterable, setIsRegisterable] = useState(false);
   const [loading, setLoading] = useState(true);
@@ -48,21 +58,21 @@ const StripeReturnPage = () => {
   useEffect(() => {
     const fetchParticipateEvent = async () => {
       const registrationResult = await participateEvent(
-        paymentIntent.TransactionAmount,
-        paymentIntent.EventId,
-        paymentIntent.NumParticipants,
-        session?.user?.user_id
+        paymentIntent?.TransactionAmount || 0,
+        paymentIntent?.EventId || "",
+        paymentIntent?.NumParticipants || 0,
+        session?.user?.user_id || "",
       );
       console.log("Registeration result:", registrationResult);
     };
 
     const fetchIsRegisterable = async () => {
       try {
-        const response = await isRegisteredEvent(session?.user?.user_id, paymentIntent.EventId);
+        const response = await isRegisteredEvent(session?.user?.user_id || "", paymentIntent?.EventId || "");
         setIsRegisterable(!response.is_registered)
         console.log("isRegistered:", response.is_registered);
         setLoading(false);
-      } catch (error) {
+      } catch (error : any) {
         // Handle the error
         console.log("Error fetching isRegisterable:", error.message);
       }
@@ -83,14 +93,14 @@ const StripeReturnPage = () => {
     <div className="h-screen w-screen flex justify-center items-center flex-col">
       <div className="bg-white text-blue-600 p-6 rounded-lg shadow-lg mx-auto font-sans">
         <h1 className="text-2xl font-bold mb-4 text-black">Payment Status</h1>
-        <p><strong>Payment Intent ID:</strong> {paymentIntent.PaymentIntentId}</p>
-        <p><strong>Transaction Amount:</strong> {paymentIntent.TransactionAmount}</p>
-        <p><strong>Currency:</strong> {paymentIntent.Currency}</p>
-        <p><strong>Status:</strong> {paymentIntent.Status}</p>
-        <p><strong>EventID:</strong> {paymentIntent.EventId}</p>
-        <p><strong>Number of guest:</strong> {paymentIntent.NumParticipants}</p>
-        <p><strong>UserID:</strong> {paymentIntent.UserId}</p>
-        {paymentIntent.Status === 'Completed' ?
+        <p><strong>Payment Intent ID:</strong> {paymentIntent?.PaymentIntentId}</p>
+        <p><strong>Transaction Amount:</strong> {paymentIntent?.TransactionAmount}</p>
+        <p><strong>Currency:</strong> {paymentIntent?.Currency}</p>
+        <p><strong>Status:</strong> {paymentIntent?.Status}</p>
+        <p><strong>EventID:</strong> {paymentIntent?.EventId}</p>
+        <p><strong>Number of guest:</strong> {paymentIntent?.NumParticipants}</p>
+        <p><strong>UserID:</strong> {paymentIntent?.UserId}</p>
+        {paymentIntent?.Status === 'Completed' ?
           <>
             <p className="text-green-500 font-bold">Complete Registering</p>
             {loading ? <LoadingLine /> : isRegisterable ? <p className="text-green-500 font-bold">Registration Successful</p> : <p className="text-red-500 font-bold">Already Registered</p>}
@@ -99,7 +109,7 @@ const StripeReturnPage = () => {
           <p className="text-yellow-500 font-bold">Pending</p>
         }
       </div>
-      <button onClick={() => router.push(`/events/${paymentIntent.EventId}`)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+      <button onClick={() => router.push(`/events/${paymentIntent?.EventId}`)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
         Go to Event
       </button>
 
