2b7a5ae9eaa82adcb31c00c9ce41a46c0fd12f4a
guncv <chanagun.vir@gmail.com>
2024-04-22 17:13:03 +0700

    fix menu pop up

--- frontend/src/app/supportandservice/layout.tsx
+++ frontend/src/app/supportandservice/layout.tsx
@@ -1,17 +1,31 @@
 import "../globals.css";
 import MenuBar from "@/components/MenuBar";
 import Menu from "@/components/Menu";
+import { getServerSession } from "next-auth";
+import getProfile from "@/libs/getProfile";
+import { authOptions } from "../api/auth/[...nextauth]/auth";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+  let profile = null;
+  
+  if (session) {
+    profile = await getProfile(session.user.user_id,session.user.token);
+  } 
+
   return (
     <html lang="en">
       <body className={`{inter.className} h-screen text-black`}>
         <MenuBar />
-        <Menu />
+        <Menu user_name={profile? profile.username: "Guest"}  
+              email={profile? profile.email : null}
+              phone_number={profile? profile.phone_number : null}
+              user_image={profile? profile.user_image : ""}
+        />
         <div className="lg:pl-[20%] md:mt-0 mt-[20px]">{children}</div>
       </body>
     </html>
